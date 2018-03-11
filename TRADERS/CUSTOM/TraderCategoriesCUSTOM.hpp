3d7c5a0f2114f075c2b4ab9db80d5711b8f35803
Neil Perrett <ned@blueyonder.co.uk>
2018-03-11 17:19:14 +0000

    updated custom with new Exile items

--- TRADERS/CUSTOM/TraderCategoriesCUSTOM.hpp
+++ TRADERS/CUSTOM/TraderCategoriesCUSTOM.hpp
@@ -5,25 +5,31 @@
 		items[] =
 		{
 			"Exile_Item_ConcreteDoorwayKit",
+			"Exile_Item_ConcreteDrawBridgeKit",
 			"Exile_Item_ConcreteFloorKit",
 			"Exile_Item_ConcreteFloorPortKit",
+			"Exile_Item_ConcreteFloorPortSmallKit",
 			"Exile_Item_ConcreteGateKit",
+			"Exile_Item_ConcreteLadderHatchKit",
 			"Exile_Item_ConcreteStairsKit",
 			"Exile_Item_ConcreteSupportKit",
 			"Exile_Item_ConcreteWallKit",
 			"Exile_Item_MetalHedgehogKit",
+			"Exile_Item_MetalLadderDoubleKit",
+			"Exile_Item_MetalLadderKit",
+			"Exile_Item_OldChestKit",
 			"Exile_Item_WoodDoorKit",
 			"Exile_Item_WoodDoorwayKit",
 			"Exile_Item_WoodFloorKit",
 			"Exile_Item_WoodFloorPortKit",
 			"Exile_Item_WoodGateKit",
+			"Exile_Item_WoodLadderHatchKit",
 			"Exile_Item_WoodStairsKit",
 			"Exile_Item_WoodSupportKit",
 			"Exile_Item_WoodWallHalfKit",
 			"Exile_Item_WoodWallKit",
 			"Exile_Item_WoodWindowKit",
 			"Exile_Item_WorkBenchKit"
-			//"Exile_Item_WoodDrawBridgeKit",
 		};
 	};
 
